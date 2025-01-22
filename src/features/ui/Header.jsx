6843c18879a4277c45e7b8ab469a814a3e860118
Alim1899 <alimakhchanogli@gmail.com>
2025-01-23 03:36:25 +0400

    Started with starter fikes

---
+++ src/features/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">Fast react Pizza Co,</Link>
+      <p>Alim</p>
+    </header>
+  );
+};
+
+export default Header;
