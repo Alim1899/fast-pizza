97a990b5a4feb7e5733f2f58cb5a3895c6499bc3
Alim1899 <alimakhchanogli@gmail.com>
2025-01-27 03:29:32 +0400

    Implemented geolocation setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,56 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
-import {
-  menuLoader,
-  orderLoader,
-  action,
-} from "./services/apiRestaurant";
-import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
-import "./index.css";
-import Error from "./ui/Error";
-import AppLayout from "./ui/AppLayout";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './ui/Home'
+import Menu from './features/menu/Menu'
+import { menuLoader, orderLoader } from './services/apiRestaurant'
+import Cart from './features/cart/Cart'
+import CreateOrder, {
+    action as createNewOrder,
+} from './features/order/CreateOrder'
+import Order from './features/order/Order'
+import './index.css'
+import Error from './ui/Error'
+import AppLayout from './ui/AppLayout'
 const router = createBrowserRouter(
-  [
-    {
-      element: <AppLayout />,
-      errorElement: <Error />,
-      children: [
-        { path: "/", element: <Home /> },
+    [
         {
-          path: "/menu",
-          element: <Menu />,
-          loader: menuLoader,
-          errorElement: <Error />,
+            element: <AppLayout />,
+            errorElement: <Error />,
+            children: [
+                { path: '/', element: <Home /> },
+                {
+                    path: '/menu',
+                    element: <Menu />,
+                    loader: menuLoader,
+                    errorElement: <Error />,
+                },
+                { path: '/cart', element: <Cart /> },
+                {
+                    path: '/order/new',
+                    element: <CreateOrder />,
+                    action: createNewOrder,
+                    errorElement: <Error />,
+                },
+                {
+                    path: '/order/:orderId',
+                    element: <Order />,
+                    loader: orderLoader,
+                    errorElement: <Error />,
+                },
+            ],
         },
-        { path: "/cart", element: <Cart /> },
-        {
-          path: "/order/new",
-          element: <CreateOrder />,
-          action: action,
-          errorElement: <Error />,
-        },
-        {
-          path: "/order/:orderId",
-          element: <Order />,
-          loader: orderLoader,
-          errorElement: <Error />,
-        },
-      ],
-    },
-  ],
-  {}
-);
+    ],
+    {}
+)
 const App = () => {
-  return (
-    <RouterProvider
-      router={router}
-      future={{
-        v7_relativeSplatPath: true,
-        v7_startTransition: true,
-      }}
-    />
-  );
-};
+    return (
+        <RouterProvider
+            router={router}
+            future={{
+                v7_relativeSplatPath: true,
+                v7_startTransition: true,
+            }}
+        />
+    )
+}
 
-export default App;
+export default App
--- src/services/apiGeocoding.js
+++ src/services/apiGeocoding.js
@@ -1,9 +1,9 @@
 export async function getAddress({ latitude, longitude }) {
-  const res = await fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
-  );
-  if (!res.ok) throw Error("Failed getting address");
+    const res = await fetch(
+        `https://api-bdc.net/data/ip-geolocation?ip=193.114.112.122&localityLanguage=en&key=bdc_81c60d7675354accab16676e42533c2e`
+    )
+    if (!res.ok) throw Error('Failed getting address')
 
-  const data = await res.json();
-  return data;
+    const data = await res.json()
+    return data
 }
